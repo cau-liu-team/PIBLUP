62b9e28219c502dabf488ece1a5a2401a4634a13
cau-liu-team <43719509+cau-liu-team@users.noreply.github.com>
2018-10-03 16:47:08 +0800

    Delete nearPD.R

--- nearPD.R
+++
@@ -1,9 +0,0 @@
-library(Matrix)
-
-nearpd <- function(gmat){
-	adg <- nearPD(gmat, ensureSymmetry=TRUE)
-	if (adg$converged == FALSE){
-		print("Warning: nearPD function (R) in the process of G matrix construction has not converged!")
-	}
-	return(as.vector(adg$mat))
-}
